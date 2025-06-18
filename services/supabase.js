bdd7d026317c27418c334190c7ac575d58ab3495
Ivan Liscovsky <liscovsky.ivan@hotmail.com>
2025-06-18 16:59:13 -0300

    FIX: Supabase y variables de entorno

--- services/supabase.js
+++ services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-import { env } from '../env.mjs';
 
-const supabaseUrl = env.supabaseUrl
-const supabaseAnonKey = env.supabaseAnonKey
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
