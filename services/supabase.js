0f57fc0e8fac910656382e7246743e09f64adb62
ivaOniria <liscovsky.ivan@gmail.com>
2025-06-26 14:48:04 -0300

    Update supabase.js

--- services/supabase.js
+++ services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rsavyhrqfdpjviikufad.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzYXZ5aHJxZmRwanZpaWt1ZmFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyMDgzNzQsImV4cCI6MjA2NTc4NDM3NH0.VfDnajVtInOctfHkvrAmnnTgwuTW7v56b17Fh7FrjjQ' 
+const supabaseUrl = EXPO_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = EXPO_PUBLIC_SUPABASE_ANON_KEY 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
